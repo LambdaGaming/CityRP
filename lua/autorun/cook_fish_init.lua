1b7752ec0ad29ff391bdaaffd59dabddca0c4349
LambdaGaming <starwars18m@gmail.com>
2019-03-18 09:44:26 -0400

    Added comments

--- lua/autorun/cook_fish_init.lua
+++ lua/autorun/cook_fish_init.lua
@@ -1,10 +1,10 @@
 
 CookFish = nil
-util.PrecacheModel( "models/props_interiors/pot02a.mdl" )
+util.PrecacheModel( "models/props_interiors/pot02a.mdl" ) --We need to precache the pot model so it can be spawned on the stove when the player uses it
 
 function CookInit(ply, before, after)
-	if after == TEAM_COOK then ply.CookFish = 0 end
+	if after == TEAM_COOK then ply.CookFish = 0 end --Init the CookFish variable for this player if they changed their job to cook
 	
-	if after != TEAM_COOK then ply.CookFish = nil end
+	if after != TEAM_COOK then ply.CookFish = nil end --Remove the CookFish variable from this player if they changed from a cook to a different job
 end
 hook.Add( "OnPlayerChangedTeam", "CookInit", CookInit )
