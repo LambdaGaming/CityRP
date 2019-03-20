8795b7f6b3a5d1cda6c9bf325339e317ac95b42e
LambdaGaming <starwars18m@gmail.com>
2019-03-20 11:27:18 -0400

    Added check to make sure only the mayor is calling these commands

--- lua/autorun/server/mayor_death_commands.lua
+++ lua/autorun/server/mayor_death_commands.lua
@@ -1,5 +1,7 @@
 
 hook.Add( "DoPlayerDeath", "MayorEndCommands", function( ply )
-	if GetGlobalBool("DarkRP_Purge") then DarkRP.endPurge( ply ) end
-	if GetGlobalBool("DarkRP_Parade") then DarkRP.endParade( ply ) end
+	if ply:Team() == TEAM_MAYOR then
+		if GetGlobalBool("DarkRP_Purge") then DarkRP.endPurge( ply ) end
+		if GetGlobalBool("DarkRP_Parade") then DarkRP.endParade( ply ) end
+	end
 end )
