6a4bb13c13f0d22df6decd54091d941e6c65827a
LambdaGaming <starwars18m@gmail.com>
2019-03-21 11:20:26 -0400

    Added comments

--- addons/Check Machine/lua/entities/check_machine/init.lua
+++ addons/Check Machine/lua/entities/check_machine/init.lua
@@ -22,17 +22,17 @@ end
 util.AddNetworkString( "CheckAccept" )
 net.Receive( "CheckAccept", function( len, ply )
 	local check = ents.Create("check")
-	check:Spawn()
+	check:Spawn() --Spawns the check once the player clicks the proceed button
 	check:SetPos( ply:GetPos() + Vector(30, 0, 0) )
 	DarkRP.notify( ply, 0, 6, "Check successfully spawned." )
-	SetGlobalInt( "Mayor_Money", GetGlobalInt( "Mayor_Money" ) - 300 )
+	SetGlobalInt( "Mayor_Money", GetGlobalInt( "Mayor_Money" ) - 300 ) --Removes 300 from the mayor funds
 end )
 
 util.AddNetworkString( "CheckMenu" )
 function ENT:Use( caller, activator )
 	if caller:IsPlayer() and !timer.Exists("Check_Timer") then
 		timer.Create("Check_Timer", cooldowntimer, 1, function() end )
-		--[[ local check = ents.Create("check")
+		--[[ local check = ents.Create("check") --Old function to spawn a check
 		check:Spawn()
 		check:SetPos( caller:GetPos() + Vector(30, 0, 0) )
 		DarkRP.notify(caller, 1, 6, "Check successfully spawned.")
@@ -41,12 +41,12 @@ function ENT:Use( caller, activator )
 				DarkRP.notify( v, 1, 6, "The check machine has just produced a check." )
 			end
 		end ]]
-		if caller:Team() == TEAM_BANKER or caller:isCP() then
-			net.Start( "CheckMenu" )
+		if caller:Team() == TEAM_BANKER or caller:isCP() then --Only allows cops and bankers to spawn checks
+			net.Start( "CheckMenu" ) --Opens the warning menu
 			net.WriteEntity( self )
 			net.Send( caller )
 		end
-	elseif timer.Exists("Check_Timer") then
+	elseif timer.Exists("Check_Timer") then --Cooldown timer, probably doesn't need to be a timer function
 		DarkRP.notify(caller, 1, 3, "Wait "..string.ToMinutesSeconds( math.Round( timer.TimeLeft( "Check_Timer" ) ) ).." minutes before spawning another check.")
 	end
 end
@@ -54,13 +54,13 @@ end
 local cooldown = 0
 function ENT:Touch( ent )
 	if cooldown > CurTime() then return end
-	if ent:GetClass() == "check" and ent.IsEventCheck then
+	if ent:GetClass() == "check" and ent.IsEventCheck then --Only passes if the check was spawned through the banker event
 		for a,ply in pairs( player.GetAll() ) do
-			if ply:Team() == TEAM_BANKER then
+			if ply:Team() == TEAM_BANKER then --Only allows the banker to deliver the check to the machine. If players wish to steal the check, they can press their use key on it to get the money
 				ply:addMoney( 400 )
 				DarkRP.notify( ply, 0, 6, "Check successfully delivered. You have been awarded $400!" )
 				ent:Remove()
-				MoneyTransferEnd()
+				MoneyTransferEnd() --Calls the end of the banker event
 			end
 		end
 		cooldown = CurTime() + 3
