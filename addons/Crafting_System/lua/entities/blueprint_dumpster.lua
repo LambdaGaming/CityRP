d0e87688bec940831a42c29a38a4156fd311785b
LambdaGaming <starwars18m@gmail.com>
2019-05-30 11:00:10 -0400

    Reduced prices

--- addons/Crafting_System/lua/entities/blueprint_dumpster.lua
+++ addons/Crafting_System/lua/entities/blueprint_dumpster.lua
@@ -10,7 +10,7 @@ ENT.AdminOnly = true
 ENT.Category = "Superadmin Only"
 
 local BLUEPRINT_CONFIG_COOLDOWN_TIME = 300
-local BLUEPRINT_CONFIG_PRICE = 5000
+local BLUEPRINT_CONFIG_PRICE = 1000
 
 BLUEPRINT_CONFIG_TIER1 = { --Blueprints you can get from the dumpster
 	{ "cw_ar15", "AR-15 (Rifle)" },
@@ -88,7 +88,7 @@ function ENT:Use( caller, activator )
 	e:Spawn()
 	e:SetEntName( randwep[1] )
 	e:SetRealName( randwep[2] )
-	caller:addMoney( -5000 )
+	caller:addMoney( -BLUEPRINT_CONFIG_PRICE )
 	timer.Create( "BlueprintCooldown", BLUEPRINT_CONFIG_COOLDOWN_TIME, 1, function() end )
 end
 
@@ -103,7 +103,7 @@ if CLIENT then
 		if self:GetPos():DistToSqr( plyShootPos ) < 562500 then
 			surface.SetFont("Bebas40Font")
 			local title = "Blueprint Dumpster"
-			local title2 = "$5000 For Each"
+			local title2 = "$1000 For Each"
 			
 			local ang = self:GetAngles()
 			ang:RotateAroundAxis( self:GetAngles():Right(),270 )
