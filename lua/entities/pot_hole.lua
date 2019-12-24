f484bb2bb1f46b714d8622bd09764ba6b71775f1
LambdaGaming <starwars18m@gmail.com>
2019-12-24 15:35:55 -0500

    Added road work event for utility worker job

---
+++ lua/entities/pot_hole.lua
@@ -0,0 +1,71 @@
+
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Pot Hole"
+ENT.Author = "Lambda Gaming"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+ENT.Category = "Superadmin Only"
+
+function ENT:SpawnFunction( ply, tr, name )
+	if !tr.Hit then return end
+	local SpawnPos = tr.HitPos + tr.HitNormal - 5
+	local ent = ents.Create( name )
+	ent:SetPos( SpawnPos )
+	ent:Spawn()
+	ent:Activate()
+	return ent
+end
+
+function ENT:Initialize()
+    self:SetModel( "models/props_debris/plaster_floor003a.mdl" )
+	self:SetMoveType( MOVETYPE_NONE )
+	self:SetSolid( SOLID_VPHYSICS )
+	if SERVER then
+		self:SetUseType( SIMPLE_USE )
+		self:SetTrigger( true )
+		self:SetHealth( 50 )
+	end
+end
+
+function ENT:Use( caller, activator )
+	if SERVER then
+		if caller:Team() == TEAM_UTILITY then
+			DarkRP.notify( caller, 0, 6, "Use a pickaxe on this pothole to even out the road surface." )
+		else
+			DarkRP.notify( caller, 0, 6, "Contact a utility worker to have them repair this pothole." )
+		end
+	end
+end
+
+function ENT:StartTouch( ent )
+	if ent:IsVehicle() and SERVER then
+		local rand = math.random( 1, 100 )
+		if rand <= 10 then
+			AM_PopTire( ent, 1 ) --10% chance of the pothole popping a tire
+		end
+	end
+end
+
+function ENT:OnTakeDamage( damage )
+	local ply = damage:GetAttacker()
+	local wep = ply:GetActiveWeapon():GetClass()
+	local pos = self:GetPos()
+	local hp = self:Health()
+	if ply:IsPlayer() and ply:Team() == TEAM_UTILITY and wep == "mgs_pickaxe" then
+		local randinterval = math.random( 1, 5 )
+		self:SetHealth( hp - randinterval )
+	end
+	if hp <= 0 then
+		self:Remove()
+		RoadWorkEnd( ply, self )
+	end
+end
+
+if CLIENT then
+    function ENT:Draw()
+        self:DrawModel()
+    end
+end
