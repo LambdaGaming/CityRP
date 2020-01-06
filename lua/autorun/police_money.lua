65a157f1b95af2caac4d6c1709bcaa5e1360b156
LambdaGaming <starwars18m@gmail.com>
2020-01-06 01:42:07 -0500

    Script error fix

--- lua/autorun/police_money.lua
+++ lua/autorun/police_money.lua
@@ -1,17 +1,17 @@
 
 local policejob = {
-	[TEAM_POLICEBOSS] = true,
-	[TEAM_OFFICER] = true,
-	[TEAM_UNDERCOVER] = true,
-	[TEAM_SWATBOSS] = true,
-	[TEAM_SWAT] = true,
-	[TEAM_FBI] = true,
-	[TEAM_BANKER] = true
+	["Police Chief"] = true,
+	["Police Officer"] = true,
+	["Undercover Officer"] = true,
+	["SWAT Commander"] = true,
+	["SWAT"] = true,
+	["Mayors Bodyguard"] = true,
+	["Banker"] = true
 }
 
 local function PoliceNotify( ply, amount, wallet )
 	for k,v in pairs( player.GetAll() ) do
-		if policejob[v:Team()] then
+		if policejob[team.GetName( v:Team() )] then
 			if amount >= 10000 then
 				DarkRP.notify(v, 1, 6, "Suspicious bank activity coming from "..ply:Nick().."." )
 			end
