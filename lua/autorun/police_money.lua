7434fc28bdeb973ca86cfd92dbab128b645770b8
LambdaGaming <starwars18m@gmail.com>
2020-07-17 23:17:15 -0400

    Added detective to bank activity notifications

--- lua/autorun/police_money.lua
+++ lua/autorun/police_money.lua
@@ -6,18 +6,17 @@ local policejob = {
 	["SWAT Commander"] = true,
 	["SWAT"] = true,
 	["Mayors Bodyguard"] = true,
-	["Banker"] = true
+	["Banker"] = true,
+	["Detective"] = true
 }
 
 local function PoliceNotify( ply, amount, wallet )
 	for k,v in pairs( player.GetAll() ) do
-		if policejob[team.GetName( v:Team() )] then
-			if amount >= 10000 then
-				DarkRP.notify(v, 1, 6, "Suspicious bank activity coming from "..ply:Nick().."." )
-			end
+		if policejob[team.GetName( v:Team() )] and amount >= 10000 then
+			DarkRP.notify(v, 1, 6, "Suspicious bank activity coming from "..ply:Nick().."." )
 		end
 	end
 end
 hook.Add( "playerWalletChanged", "PoliceNotify", PoliceNotify )
 
-MsgC( color_orange, "[CityRP] Loaded player wallet police notification." )
+MsgC( color_red, "[CityRP] Loaded player wallet police notification." )
