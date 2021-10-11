47f1fb0413c8d21ef9960448aeece382a6070242
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2021-10-10 22:50:16 -0400

    Fixed parade and purge not ending if mayor left the server

--- lua/autorun/server/mayor_death_commands.lua
+++ lua/autorun/server/mayor_death_commands.lua
@@ -1,9 +1,8 @@
-
-hook.Add( "DoPlayerDeath", "MayorEndCommands", function( ply )
+local function EndEvents( ply )
 	if ply:Team() == TEAM_MAYOR then
 		if GetGlobalBool( "DarkRP_Purge" ) then DarkRP.endPurge( ply ) end
 		if GetGlobalBool( "DarkRP_Parade" ) then DarkRP.endParade( ply ) end
 	end
-end )
-
-MsgC( color_orange, "[CityRP] Loaded mayor death initialization." )
+end
+hook.Add( "DoPlayerDeath", "MayorEndCommands", EndEvents )
+hook.Add( "PlayerDisconnected", "MayorEndCommandsDisconnect", EndEvents )
