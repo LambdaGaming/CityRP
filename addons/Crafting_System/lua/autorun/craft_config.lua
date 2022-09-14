59673ec449708899f3fc1d52378108cc17e2938e
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2022-09-13 21:45:29 -0400

    Updated crafting table config

--- addons/Crafting_System/lua/autorun/craft_config.lua
+++ addons/Crafting_System/lua/autorun/craft_config.lua
@@ -1,4 +1,3 @@
-
 CRAFT_CONFIG_MAXHEALTH = 100
 
 CRAFT_CONFIG_MODEL = "models/props_wasteland/controlroom_desk001b.mdl"
@@ -28,3 +27,33 @@ CRAFT_CONFIG_MENU_COLOR = Color( 49, 53, 61, 200 )
 CRAFT_CONFIG_BUTTON_COLOR = Color( 230, 93, 80, 255 )
 
 CRAFT_CONFIG_BUTTON_TEXT_COLOR = color_white
+
+CRAFT_CONFIG_TREE_HEALTH = 100
+
+CRAFT_CONFIG_TREE_RESPAWN = 300
+
+CRAFT_CONFIG_ROCK_HEALTH = 100
+
+CRAFT_CONFIG_ROCK_RESPAWN = 300
+
+CRAFT_CONFIG_ROCK_MODELS = {
+	"models/props/cs_militia/militiarock05.mdl",
+	"models/props/cs_militia/militiarock03.mdl",
+	"models/props/cs_militia/militiarock02.mdl"
+}
+
+CRAFT_CONFIG_TREE_MODELS = {
+	"models/props/CS_militia/tree_large_militia.mdl"
+}
+
+CRAFT_CONFIG_TREE_INGREDIENTS = {
+	{ "wood", 100 }
+}
+
+CRAFT_CONFIG_MINE_WHITELIST_TREE = {
+	["swm_axe"] = true
+}
+
+CRAFT_CONFIG_MINE_WHITELIST_ROCK = {
+	["mgs_pickaxe"] = true
+}
