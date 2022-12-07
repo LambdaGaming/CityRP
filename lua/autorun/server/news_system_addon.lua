5ed700a1d6684e8c947c5e0820deff8791aaa66e
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2022-12-07 02:32:17 -0500

    Don't pay news cameraman for his own tv

--- lua/autorun/server/news_system_addon.lua
+++ lua/autorun/server/news_system_addon.lua
@@ -1,4 +1,4 @@
-local newspayamount = 0
+local prefixlist = { "/", "!", "@" }
 
 timer.Create( "news_pay_loop", 60, 0, function()
 	if team.NumPlayers( TEAM_CAMERA ) <= 0 then return end
@@ -8,19 +8,26 @@ timer.Create( "news_pay_loop", 60, 0, function()
 			DarkRP.notify( v, 1, 6, "You have received no news TV profits because nobody is tuned to your channel." )
 			return
 		end
-		newspayamount = 50 * tvs
-		v:addMoney( newspayamount )
-		DarkRP.notify( v, 0, 6, "You have received $"..newspayamount.." from news TV profits." )
+
+		local total = 0
+		for k,v in pairs( tvs ) do
+			if v:GetOwner() != v then
+				total = total + 50
+			end
+		end
+		v:addMoney( total )
+		DarkRP.notify( v, 0, 6, "You have received $"..total.." from news TV profits." )
 	end
 end )
 
 hook.Add( "PlayerSay", "NewsText", function( ply, text, teamChat )
 	if teamChat then return end
-	local prefixlist = { "/", "!", "@" }
     if table.HasValue( prefixlist, string.sub( text, 0, 1 ) ) then return end
+	local tvs = ents.FindByClass( "news_tv" )
+	if #tvs < 1 then return end
 	for c,d in pairs( ents.FindInSphere( ply:GetPos(), 150 ) ) do
 		if d:IsPlayer() and d:Team() == TEAM_CAMERA and d:GetActiveWeapon():GetClass() == "news_camera" then
-			for a,b in pairs( ents.FindByClass( "news_tv" ) ) do
+			for a,b in pairs( tvs ) do
 				if !b:GetOn() or b:GetChannel() == 0 then return end
 				for k,v in pairs( ents.FindInSphere( b:GetPos(), 150 ) ) do
 					if v:IsPlayer() then
