79272005cc5742d177f901124fc85ce3fb0f23df
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2022-12-17 00:26:22 -0500

    Only reward those who damage npc during shooter event

--- addons/Event System/lua/autorun/server/event_shooter.lua
+++ addons/Event System/lua/autorun/server/event_shooter.lua
@@ -1,6 +1,5 @@
-local map = game.GetMap()
-
 function ActiveShooter()
+	local map = game.GetMap()
 	local shooter = ents.Create( "npc_citizen" )
 	shooter:SetPos( table.Random( EventPos[map].Shooter ) )
 	shooter:Spawn()
@@ -11,6 +10,7 @@ function ActiveShooter()
 		shooter:AddEntityRelationship( v, D_HT, 99 )
 	end
 	shooter.IsEventNPC = true
+	shooter.Participants = {}
 	
 	SetGlobalBool( "EventActive", true )
 	SetGlobalString( "ActiveEvent", "Active Shooter" )
@@ -21,23 +21,10 @@ function ActiveShooter()
 	end
 end
 
-function ActiveShooterEnd( killer )
-	local numcp = 1
-	for k,v in ipairs( player.GetAll() ) do
-		if v:isCP() and v:Team() != TEAM_MAYOR then
-			numcp = numcp + 1
-		end
-	end
-	local reward = 12000 + ( numcp * 4000 )
-	DarkRP.notify( killer, 0, 10, "You have been given $"..reward.." and a crafting blueprint for stopping the threat." )
-	GiveReward( killer, reward )
-	if numcp > 1 then
-		DarkRP.notify( killer, 2, 10, "Please distribute these earnings among those who helped you." )
-	end
-	for k,v in ipairs( player.GetAll() ) do
-		if v:isCP() then
-			DarkRP.notify( v, 0, 10, "The active shooter has been dealt with and is no longer a threat!" )
-		end
+function ActiveShooterEnd( npc )
+	for k,v in pairs( npc.Participants ) do
+		DarkRP.notify( v, 0, 10, "You have been given $12,000 and a crafting blueprint for helping stop the threat." )
+		GiveReward( v, 12000 )
 	end
 	ActiveEvents[EVENT_ACTIVE_SHOOTER] = false
 end
@@ -54,39 +41,42 @@ end
 hook.Add( "PlayerInitialSpawn", "ActiveShooterRelationship", ActiveShooterRelationship )
 
 local function ShooterKilled( npc, attacker, inflictor )
-	if npc:GetClass() == "npc_citizen" then
-		if npc.IsEventNPC then
-			ActiveShooterEnd( attacker )
-			for k,v in pairs( ents.FindInSphere( npc:GetPos(), 50 ) ) do
-				if v:GetClass() == "weapon_smg1" then
-					v:Remove()
-				end
+	if npc.IsEventNPC then
+		ActiveShooterEnd( npc )
+		for k,v in pairs( ents.FindInSphere( npc:GetPos(), 50 ) ) do
+			if v:GetClass() == "weapon_smg1" then
+				v:Remove()
 			end
 		end
-		if npc.IsRobber then
-			for k,v in pairs( ents.FindInSphere( npc:GetPos(), 50 ) ) do
-				timer.Simple( 1, function()
-					if IsValid( v ) then
-						local weplist = {
-							["weapon_smg1"] = true,
-							["weapon_pistol"] = true,
-							["weapon_shotgun"] = true
-						}
-						if weplist[v:GetClass()] and !IsValid( v:GetOwner() ) then
-							v:Remove()
-						end
+	elseif npc.IsRobber then
+		for k,v in pairs( ents.FindInSphere( npc:GetPos(), 50 ) ) do
+			timer.Simple( 1, function()
+				if IsValid( v ) then
+					local weplist = {
+						["weapon_smg1"] = true,
+						["weapon_pistol"] = true,
+						["weapon_shotgun"] = true
+					}
+					if weplist[v:GetClass()] and !IsValid( v:GetOwner() ) then
+						v:Remove()
 					end
-				end )
-			end
-			if attacker:IsPlayer() then
-				DarkRP.notify( attacker, 0, 6, "You have been rewarded with $1500 and a crafting blueprint for killing a robber." )
-				GiveReward( attacker, 1500 )
-			end
-			RobberyEventStats.RobberCount = RobberyEventStats.RobberCount - 1
-			if RobberyEventStats.RobberCount == 0 then
-				RobberyEnd()
-			end
+				end
+			end )
+		end
+		if attacker:IsPlayer() then
+			DarkRP.notify( attacker, 0, 6, "You have been rewarded with $1500 and a crafting blueprint for killing a robber." )
+			GiveReward( attacker, 1500 )
+		end
+		RobberyEventStats.RobberCount = RobberyEventStats.RobberCount - 1
+		if RobberyEventStats.RobberCount == 0 then
+			RobberyEnd()
 		end
 	end
 end
 hook.Add( "OnNPCKilled", "ShooterKilled", ShooterKilled )
+
+hook.Add( "EntityTakeDamage", "ShooterCountDamage", function( target, dmg )
+	if target.IsEventNPC and !table.HasValue( target.Participants, dmg:GetAttacker() ) then
+		table.insert( target.Participants, dmg:GetAttacker() )
+	end
+end )
