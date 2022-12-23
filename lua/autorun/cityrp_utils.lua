54f150b5e04c087d65f7dbb996244903077d5bf9
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2022-12-23 17:24:17 -0500

    Merge DrawNPCText function with utils file

--- lua/autorun/cityrp_utils.lua
+++ lua/autorun/cityrp_utils.lua
@@ -83,6 +83,33 @@ if SERVER then
 		local day = StormFox2.Time.IsDay()
 		StormFox2.Time.Set( day and "12:00 AM" or "12:00 PM" )
 	end )
+else
+	--NPC text drawing function, modified from https://github.com/Bhoonn/bh_accessories/blob/main/lua/entities/bh_acc_vendor/cl_init.lua
+	local ent = FindMetaTable( "Entity" )
+	local offset = Vector( 0, 0, 80 )
+	local Draw_SimpleText = draw.SimpleText
+	local surface_SetFont = surface.SetFont
+	local surface_GetTextSize = surface.GetTextSize
+
+	function ent:DrawNPCText( text, override )
+		self:DrawModel()
+
+		local origin = self:GetPos()
+		local ply = LocalPlayer()
+		if ply:GetPos():DistToSqr( origin ) >= ( 768 * 768 ) then return end
+
+		local pos = origin + ( override or offset )
+		local ang = ( ply:EyePos() - pos ):Angle()
+		ang.p = 0
+		ang:RotateAroundAxis( ang:Right(), 90 )
+		ang:RotateAroundAxis( ang:Up(), 90 )
+		ang:RotateAroundAxis( ang:Forward(), 180 )
+		cam.Start3D2D(pos, ang, 0.035)
+			surface_SetFont( "BHACC_VendorTextFont" )
+			local tw, th = surface_GetTextSize( text )
+			Draw_SimpleText( text, "BHACC_VendorTextFont", 0, 0, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP )
+		cam.End3D2D()
+	end
 end
 
 local meta = FindMetaTable( "Player" )
