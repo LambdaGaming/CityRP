977cbc9168aae27619f0d4c664c707320a1a7713
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2023-02-15 22:52:56 -0500

    Finish eco revamp

--- addons/Economy System/lua/autorun/economy_utils.lua
+++ addons/Economy System/lua/autorun/economy_utils.lua
@@ -1,50 +1,94 @@
-if SERVER then
-	EconomyPerks = {}
-
-	local function CalculateEcoPoints()
-		local income = math.Clamp( math.Round( ( GetCityIncome() + 5000 ) * 25 / 5000 - 25 ), -25, 25 )
-		local funds = math.Clamp( math.Round( ( GetVaultAmount() + 50000 ) * 25 / 100000 - 25 ), -25, 25 )
-		return income + funds
-	end
-
-	function SetVaultAmount( amount )
-		SetGlobalInt( "CityVaultMoney", amount )
-		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
-	end
-
-	function AddVaultFunds( amount )
-		SetGlobalInt( "CityVaultMoney", GetGlobalInt( "CityVaultMoney" ) + amount )
-		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
-	end
-
-	function SetCityIncome( amount )
-		SetGlobalInt( "CityIncome", amount )
-		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
-	end
-
-	function AddCityIncome( amount )
-		SetGlobalInt( "CityIncome", GetGlobalInt( "CityIncome" ) + amount )
-		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
-	end
-
-	SetVaultAmount( 50000 )
-	timer.Create( "CityIncomeLoop", 300, 0, function()
-		local income = GetCityIncome()
-		if income > 0 then
-			local mayor = team.GetPlayers( TEAM_MAYOR )[1]
-			AddVaultFunds( income )
-		end
-	end )
-end
-
 function GetVaultAmount()
 	return GetGlobalInt( "CityVaultMoney" )
 end
 
 function GetCityIncome()
-	return GetGlobalInt( "CityIncome" )
+	local income = GetGlobalInt( "CityIncome" )
+	local govmoney = GetGlobalInt( "CityGovPayout" )
+	return income + govmoney
 end
 
 function GetEcoPoints()
 	return GetGlobalInt( "CityEcoPoints" )
+end
+
+if SERVER then
+	EconomyPerks = {}
+
+	local function CalculateEcoPoints()
+		local income = math.Clamp( math.Round( ( GetCityIncome() + 5000 ) * 25 / 5000 - 25 ), -25, 25 )
+		local funds = math.Clamp( math.Round( ( GetVaultAmount() + 50000 ) * 25 / 75000 - 25 ), -25, 25 )
+		return income + funds
+	end
+
+	local function CheckMayorModel()
+		if GetEcoPoints() >= 45 then
+			local mayor = team.GetPlayers( TEAM_MAYOR )[1]
+			mayor:SetModel( "models/Gigabreen/Gigabreen.mdl" )
+		end
+	end
+
+	local function UpdateGovPayout()
+		local govmoney = 0
+		for k,v in ipairs( player.GetAll() ) do
+			if v:isCP() then
+				govmoney = govmoney - DarkRP.retrieveSalary( v )
+			end
+		end
+		SetGlobalInt( "CityGovPayout", govmoney )
+	end
+
+	function SetVaultAmount( amount )
+		SetGlobalInt( "CityVaultMoney", amount )
+		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
+		CheckMayorModel()
+	end
+
+	function AddVaultFunds( amount )
+		SetGlobalInt( "CityVaultMoney", GetGlobalInt( "CityVaultMoney" ) + amount )
+		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
+		CheckMayorModel()
+	end
+
+	function SetCityIncome( amount )
+		SetGlobalInt( "CityIncome", amount )
+		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
+		CheckMayorModel()
+	end
+
+	function AddCityIncome( amount )
+		SetGlobalInt( "CityIncome", GetGlobalInt( "CityIncome" ) + amount )
+		SetGlobalInt( "CityEcoPoints", CalculateEcoPoints() )
+		CheckMayorModel()
+	end
+
+	SetVaultAmount( 25000 )
+	timer.Create( "CityIncomeLoop", 300, 0, function()
+		local income = GetCityIncome()
+		if income != 0 then
+			local mayor = team.GetPlayers( TEAM_MAYOR )[1]
+			local term = income > 0 and "gained" or "lost"
+			AddVaultFunds( income )
+			if IsValid( mayor ) then
+				DarkRP.notify( mayor, 0, 6, "The city has "..term.." "..DarkRP.formatMoney( income ) )
+			end
+		end
+	end )
+
+	hook.Add( "playerGetSalary", "ModifyGovSalary", function( ply, amount )
+		if ply:isCP() then
+			if amount > GetVaultAmount() then
+				return false, "You do not receive a paycheck this cycle because the city is out of money.", 0
+			end
+			AddVaultFunds( -amount )
+		end
+	end )
+
+	hook.Add( "OnPlayerChangedTeam", "UpdateCityIncome", function()
+		UpdateGovPayout()
+	end )
+
+	hook.Add( "PlayerDisconnected", "UpdateCityIncomeDisconnect", function()
+		UpdateGovPayout()
+	end )
 end
