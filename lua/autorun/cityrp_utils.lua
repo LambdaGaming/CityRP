c7d95d582dbd8bea0d98abb7664ae4cb5349a05f
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-11-28 10:43:32 -0500

    Cleanup NPC text function

--- lua/autorun/cityrp_utils.lua
+++ lua/autorun/cityrp_utils.lua
@@ -133,9 +133,11 @@ else
 	--NPC text drawing function, modified from https://github.com/Bhoonn/bh_accessories/blob/main/lua/entities/bh_acc_vendor/cl_init.lua
 	local ent = FindMetaTable( "Entity" )
 	local offset = Vector( 0, 0, 80 )
-	local Draw_SimpleText = draw.SimpleText
-	local surface_SetFont = surface.SetFont
-	local surface_GetTextSize = surface.GetTextSize
+	surface.CreateFont( "NPCOverheadText", {
+		font = "Circular Std Bold",
+		size = 200,
+		weight = 800
+	} )
 
 	function ent:DrawNPCText( text, override )
 		self:DrawModel()
@@ -151,9 +153,7 @@ else
 		ang:RotateAroundAxis( ang:Up(), 90 )
 		ang:RotateAroundAxis( ang:Forward(), 180 )
 		cam.Start3D2D( pos, ang, 0.035 )
-			surface_SetFont( "BHACC_VendorTextFont" )
-			local tw, th = surface_GetTextSize( text )
-			Draw_SimpleText( text, "BHACC_VendorTextFont", 0, 0, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP )
+			draw.SimpleText( text, "NPCOverheadText", 0, 0, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP )
 		cam.End3D2D()
 	end
 end
