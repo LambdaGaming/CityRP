fef8e9bd2e6fcd2d714e3bfc957d326c33ff9177
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-05-15 12:52:40 -0400

    Merge evidence wand with metal detector

--- cityrp misc scripts/lua/weapons/weapon_metal_detector.lua
+++ cityrp misc scripts/lua/weapons/weapon_metal_detector.lua
@@ -1,14 +1,16 @@
 AddCSLuaFile()
 
-SWEP.PrintName = "Evidence Wand"
-SWEP.Category = "Evidence"
+SWEP.PrintName = "Metal Detector"
+SWEP.Category = "DarkRP"
 SWEP.Spawnable = true
 SWEP.AdminOnly = true
 SWEP.Base = "weapon_base"
-SWEP.Author = "Lambda Gaming"
+SWEP.Author = "OPGman"
 SWEP.Slot = 3
-SWEP.ViewModel = "models/weapons/v_stunbaton.mdl"
-SWEP.WorldModel = "models/weapons/w_stunbaton.mdl"
+SWEP.ViewModel = "models/weapons/Custom/scanner.mdl"
+SWEP.WorldModel = "models/weapons/Custom/w_scanner.mdl"
+SWEP.UseHands = true
+SWEP.HoldType = "Pistol"
 
 SWEP.Primary.Ammo = "none"
 SWEP.Primary.ClipSize = -1
@@ -19,15 +21,29 @@ SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 
-local wepWhitelist = {
+local allowed = {
+	["itemstore_pickup"] = true,
 	["keys"] = true,
 	["weapon_physcannon"] = true,
 	["gmod_camera"] = true,
 	["gmod_tool"] = true,
+	["pocket"] = true,
 	["weapon_physgun"] = true,
+	["weapon_keypadchecker"] = true,
+	["weapon_hl2pickaxe"] = true,
+	["weapon_hl2axe"] = true,
 	["rphands"] = true,
-	["itemstore_pickup"] = true,
-	["weapon_handcuffed"] = true
+	["weapon_handcuffed"] = true,
+	["fish_finder"] = true,
+	["fishing_rod_physics"] = true,
+	["weapon_extinguisher"] = true,
+	["usm_c4"] = true,
+	["weapon_slam"] = true,
+	["weapon_car_bomb"] = true,
+	["weapon_vfire_molly"] = true,
+	["weapon_vfire_gascan"] = true,
+	["weapon_agent"] = true,
+	["weapon_metal_detector"] = true
 }
 
 local function CanConfiscateFrom( ply, target )
@@ -40,36 +56,66 @@ local function CanConfiscateFrom( ply, target )
 end
 
 function SWEP:PrimaryAttack()
-	if !IsFirstTimePredicted() or CLIENT then return end
-	local ply = self:GetOwner()
-    local tr = ply:GetEyeTrace().Entity
-
-	if tr:IsPlayer() then
-		if !CanConfiscateFrom( ply, tr ) then return end
-		local foundwep = false
-		local plyweps = tr:GetWeapons()
-		for k,v in pairs( plyweps ) do
-			if !wepWhitelist[v:GetClass()] and !table.HasValue( tr:getJobTable().weapons, v:GetClass() ) then
-				foundwep = true
-				break
+	self:SetWeaponAnim( ACT_VM_PRIMARYATTACK )
+	self:SetNextPrimaryFire( CurTime() + 1 )
+	self:SetNextSecondaryFire( CurTime() + 1 )
+	if SERVER then
+		local tr = ply:GetEyeTrace().Entity
+		local owner = self:GetOwner()
+		if !tr:IsPlayer() or owner:GetPos():DistToSqr( tr:GetPos() ) > 10000 then return end
+		local weps = tr:GetWeapons()
+		local found = {}
+		for k,v in pairs( weps ) do
+			local class = v:GetClass()
+			if !allowed[class] and !table.HasValue( tr:getJobTable().weapons, class ) then
+				table.insert( found, v:GetPrintName() )
 			end
 		end
-		if foundwep then
-			ply:EmitSound( "weapons/stunstick/alyx_stunner2.wav" )
-			net.Start( "ViewWeapons" )
-			net.WriteEntity( tr )
-			net.WriteTable( plyweps )
-			net.Send( self:GetOwner() )
+		if table.IsEmpty( found ) then
+			owner:ChatPrint( "No weapons detected." )
 		else
-			ply:EmitSound( "buttons/combine_button_locked.wav" )
-			DarkRP.notify( self:GetOwner(), 1, 6, "No illegal weapons detected." )
+			local txt = ""
+			for i = 1, #found do
+				if i != #found then
+					txt = txt..found[i]..", "
+				else
+					txt = txt..found[i]
+				end
+			end
+			owner:ChatPrint( "Weapons found!" )
+			owner:ChatPrint( txt )
+			owner:EmitSound( "buttons/blip2.wav" )
 		end
-		self:SetNextPrimaryFire( CurTime() + 1 )
-		return
 	end
 end
 
 function SWEP:SecondaryAttack()
+	if !IsFirstTimePredicted() or CLIENT then return end
+	local ply = self:GetOwner()
+    local tr = ply:GetEyeTrace().Entity
+	if !CanConfiscateFrom( ply, tr ) then return end
+	local foundwep = false
+	local weps = tr:GetWeapons()
+	for k,v in pairs( weps ) do
+		local class = v:GetClass()
+		if !allowed[class] and !table.HasValue( tr:getJobTable().weapons, class ) then
+			foundwep = true
+			break
+		end
+	end
+	if foundwep then
+		ply:EmitSound( "weapons/stunstick/alyx_stunner2.wav" )
+		net.Start( "ViewWeapons" )
+		net.WriteEntity( tr )
+		net.WriteTable( weps )
+		net.Send( self:GetOwner() )
+	else
+		ply:EmitSound( "buttons/combine_button_locked.wav" )
+		DarkRP.notify( self:GetOwner(), 1, 6, "No weapons detected." )
+	end
+	self:SetNextPrimaryFire( CurTime() + 1 )
+	self:SetNextSecondaryFire( CurTime() + 1 )
+	return
 end
 
 if CLIENT then
@@ -77,7 +123,7 @@ if CLIENT then
 		local ply = net.ReadEntity()
 		local weps = net.ReadTable()
 		local mainframe = vgui.Create( "DFrame" )
-		mainframe:SetTitle( "Illegal weapons:" )
+		mainframe:SetTitle( "Found weapons:" )
 		mainframe:SetSize( 200, 300 )
 		mainframe:Center()
 		mainframe:MakePopup()
@@ -87,7 +133,8 @@ if CLIENT then
 		local mainframescroll = vgui.Create( "DScrollPanel", mainframe )
 		mainframescroll:Dock( FILL )
 		for k,v in pairs( weps ) do
-			if wepWhitelist[v:GetClass()] or table.HasValue( ply:getJobTable().weapons, v:GetClass() ) then
+			local class = v:GetClass()
+			if allowed[class] or table.HasValue( ply:getJobTable().weapons, class ) then
 				continue
 			end
 			local scrollbutton = vgui.Create( "DButton", mainframescroll )
