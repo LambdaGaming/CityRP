eefa2b45ead3e862195bbd46b8bfcba8a51632be
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-05-20 19:25:10 -0400

    Finish drug selling system

---
+++ Drug System/lua/entities/drug_box.lua
@@ -0,0 +1,52 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Drug Box"
+ENT.Author = "OPGman"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+ENT.Category = "Drugs"
+
+function ENT:Initialize()
+	self:SetModel( "models/props/CS_militia/crate_extrasmallmill.mdl" )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	if SERVER then
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetUseType( SIMPLE_USE )
+		self:SetTrigger( true )
+		self.Drugs = {}
+		self.MaxDrugs = 5
+	end
+	self:PhysWake()
+end
+
+local drugs = {
+	["drug_weed"] = true,
+	["drug_cocaine"] = true,
+	["drug_meth"] = true
+}
+
+function ENT:StartTouch( ent )
+	local class = ent:GetClass()
+	if !drugs[class] or table.Count( self.Drugs ) >= self.MaxDrugs then return end
+	local stat
+	if class == "drug_weed" then
+		stat = ent:GetWeedType()
+	elseif class == "drug_cocaine" then
+		stat = ent:GetPurity()
+	else
+		stat = ent:GetIngredients()
+	end
+	table.insert( self.Drugs, { Class = class, Stat = stat } )
+	ent:Remove()
+	self:EmitSound( "physics/cardboard/cardboard_box_impact_soft"..math.random( 1, 7 )..".wav" )
+end
+
+if CLIENT then
+	local offset = Vector( 0, 0, 60 )
+	function ENT:Draw()
+		self:DrawNPCText( "Drug Box", offset )
+	end
+end
--- Drug System/lua/entities/smuggle_sell.lua
+++ Drug System/lua/entities/smuggle_sell.lua
@@ -17,10 +17,10 @@ function ENT:Initialize()
 		self:SetCollisionGroup( COLLISION_GROUP_PLAYER )
 	end
 	self:PhysWake()
+	self.DrugCooldowns = {}
 end
 
 function ENT:Use( ply )
-	local foundItem = false
 	local pos = self:GetPos()
 	local numply = 0
 	for k,v in ipairs( ents.FindInSphere( pos, 500 ) ) do
@@ -33,8 +33,41 @@ function ENT:Use( ply )
 		return
 	end
 	for k,v in ipairs( ents.FindInSphere( pos, 500 ) ) do
-		--TODO: Drug stuff
-		if v.SmuggleTruck then
+		if v:GetClass() == "drug_box" then
+			local id = ply:SteamID64()
+			if table.IsEmpty( v.Drugs ) then
+				DarkRP.notify( ply, 1, 6, "You brought an empty box? Nice try." )
+				return
+			end
+			if self.DrugCooldowns[id] and self.DrugCooldowns[id] > CurTime() then
+				DarkRP.notify( ply, 1, 6, "Please wait for the cooldown to end before selling more drugs." )
+				return
+			end
+			local payout = 0
+			for a,b in pairs( v.Drugs ) do
+				if b.Class == "drug_weed" then
+					if b.Stat == 2 then
+						--Spicy weed
+						payout = payout + 1000
+					else
+						payout = payout + 200
+					end
+				elseif b.Class == "drug_cocaine" then
+					--Payout based on purity
+					payout = payout + ( b.Stat * 50 )
+				else
+					--Payout based on ingredient amount
+					payout = payout + ( b.Stat * 500 )
+				end
+			end
+			if ply:isCP() then
+				self.DrugCooldowns[id] = CurTime() + 600
+			end
+			ply:addMoney( payout )
+			DarkRP.notify( ply, 0, 6, "You have sold a box of drugs for "..DarkRP.formatMoney( payout ).."." )
+			v:Remove()
+			return
+		elseif v.SmuggleTruck then
 			if !ply:IsCivilian() then
 				DarkRP.notify( ply, 1, 6, "Only civilians can smuggle things!" )
 				return
@@ -43,10 +76,9 @@ function ENT:Use( ply )
 			v:Remove()
 			return
 		elseif v:GetClass() == "custom_shipment" and v.Ready and v:GetOwner() != ply then
-			foundent:Remove()
-			local amt = math.Round( foundent:GetAmount() / 2 )
+			local amt = math.Round( v:GetAmount() * 0.5 )
 			for i=1,amt do
-				local class = ShipmentWepList[foundent:GetGunType()][1]
+				local class = ShipmentWepList[v:GetGunType()][1]
 				local e = ents.Create( "spawned_weapon" )
 				e:SetWeaponClass( class )
 				e:SetModel( weapons.GetStored( class ).WorldModel )
@@ -54,13 +86,12 @@ function ENT:Use( ply )
 				e.nodupe = true
 				e:Spawn()
 			end
+			v:Remove()
 			DarkRP.notify( ply, 0, 6, "Successfully smuggled a weapon shipment." )
 			return
 		end
 	end
-	if !foundItem then
-		DarkRP.notify( ply, 1, 6, "No item detected. Try moving it closer." )
-	end
+	DarkRP.notify( ply, 1, 6, "No item detected. Try moving it closer." )
 end
 
 if CLIENT then
